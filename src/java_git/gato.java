6b79921c24d666b09b7833959667bd9e7538cd07
Santiago <santi.ardaiz@gmail.com>
2023-03-23 22:06:56 -0300

    Se agrego gato

---
+++ src/java_git/gato.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_git;
+
+/**
+ *
+ * @author santi
+ */
+public class gato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] gato = {"Independiente", "curioso", "ágil", "peludo", "territorial", "bigotudo", "ortiva"};
+    }
+
+}
